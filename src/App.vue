b4f41cc52eff9ea6d984ce72cd534e936625971f
Artem <moose.2000@mail.ru>
2021-11-14 01:04:11 +0300

    251 / Добавлена логика в работу суфлера, релиз

--- src/App.vue
+++ src/App.vue
@@ -5,43 +5,57 @@
         <article class="teleprompter__card">
           <div class="teleprompter__inner">
             <div class="teleprompter__window">
-              <textarea class="teleprompter__textarea">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+              <textarea class="teleprompter__textarea"
+                        ref="textarea"
+                        :style="styleObject"
+                        :value="dataText.text.message"
+              >
               </textarea>
             </div>
-            <ul class="teleprompter__settings list">
-              <li class="teleprompter__settings-block">
-                <button class="teleprompter__button button button--default">
-                  Включить
-                </button>
-              </li>
-              <li class="teleprompter__settings-block">
-                <button class="teleprompter__button button button--default is-disabled disabled">
-                  <span>Пауза</span>
-                </button>
-              </li>
-              <li class="teleprompter__settings-block">
-                <button class="teleprompter__button button button--default is-disabled disabled">
-                  Рестарт
-                </button>
-              </li>
-              <li class="teleprompter__settings-block form-field form-field--short">
-                <input type="number" class="teleprompter__input form-field__input"
-                       max="2" min="1" step="0.5" value="1">
-                <label class="teleprompter__label form-field__label">&plusmn;Скорость</label>
-              </li>
-              <li class="teleprompter__settings-block form-field form-field--short">
-                <input type="number" class="teleprompter__input form-field__input"
-                       max="32" min="16" value="16">
-                <label class="teleprompter__label form-field__label">&plusmn;Шрифт</label>
-              </li>
-            </ul>
+            <div class="teleprompter__settings">
+              <ul class="teleprompter__settings-list list">
+                <li class="teleprompter__settings-item">
+                  <button class="teleprompter__button button button--default"
+                          @click="scrollTop(currentPosition)"
+                          :class="[flagRun ? 'isActive' : '']"
+                          :disabled="flagRun"
+                  >
+                    Включить
+                  </button>
+                </li>
+                <li class="teleprompter__settings-item">
+                  <button class="teleprompter__button button button--default"
+                          @click="pauseScroll"
+                          :class="{isActive: flagPause}"
+                  >
+                    <span>Пауза</span>
+                  </button>
+                </li>
+                <li class="teleprompter__settings-item">
+                  <button class="teleprompter__button button button--default"
+                          @click="restartScroll"
+                  >
+                    Рестарт
+                  </button>
+                </li>
+              </ul>
+              <ul class="teleprompter__settings-list list">
+                <li class="teleprompter__settings-item form-field form-field--short">
+                  <input type="number" class="teleprompter__input form-field__input"
+                         max="2" min="1" step="0.5"
+                         v-model.number="speedText"
+                  >
+                  <label class="teleprompter__label form-field__label">&plusmn;Скорость</label>
+                </li>
+                <li class="teleprompter__settings-item form-field form-field--short">
+                  <input type="number" class="teleprompter__input form-field__input"
+                         max="32" min="24"
+                         v-model.number="fontSize"
+                  >
+                  <label class="teleprompter__label form-field__label">&plusmn;Шрифт</label>
+                </li>
+              </ul>
+            </div>
           </div>
         </article>
       </section>
@@ -50,12 +64,56 @@
 </template>
 
 <script>
+import dataText from './data/text';
+
 export default {
   name: 'App',
   data() {
     return {
-
+      speedText: 1,
+      fontSize: 24,
+      currentPosition: null,
+      breakScroll: null,
+      flagRun: false,
+      flagPause: false,
+      dataText,
     };
+  },
+  methods: {
+    scrollTop() {
+      this.flagRun = true;
+      this.flagPause = false;
+      const $textarea = this.$refs.textarea;
+      this.breakScroll = setInterval(() => {
+        this.currentPosition += this.speedText;
+        $textarea.scrollTo({
+          top: this.currentPosition,
+        });
+      }, 10);
+    },
+    pauseScroll() {
+      this.flagRun = false;
+      this.flagPause = true;
+      clearInterval(this.breakScroll);
+    },
+    restartScroll() {
+      this.flagRun = false;
+      this.flagPause = false;
+      const $textarea = this.$refs.textarea;
+      this.currentPosition = 0;
+      $textarea.scrollTo({
+        top: this.currentPosition,
+        behavior: 'smooth',
+      });
+      clearInterval(this.breakScroll);
+    },
+  },
+  computed: {
+    styleObject() {
+      return {
+        fontSize: `${this.fontSize}px`,
+      };
+    },
   },
 };
 </script>
