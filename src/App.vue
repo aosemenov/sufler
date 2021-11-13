035016072a77aff849a91d0238bb38152c2afa21
Artem <moose.2000@mail.ru>
2021-11-13 17:49:46 +0300

    251 / Создание структуры преокта

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <section class="teleprompter">
+        <article class="teleprompter__card">
+          <div class="teleprompter__inner">
+            <div class="teleprompter__window">
+              <textarea class="teleprompter__textarea">
+
+              </textarea>
+            </div>
+            <ul class="teleprompter__settings list">
+              <li class="teleprompter__settings-block">
+                <button class="teleprompter__button button button--default">
+                  Включить
+                </button>
+              </li>
+              <li class="teleprompter__settings-block">
+                <button class="teleprompter__button button button--default">
+                  Пауза
+                </button>
+              </li>
+              <li class="teleprompter__settings-block">
+                <button class="teleprompter__button button button--default">
+                  Рестарт
+                </button>
+              </li>
+              <li class="teleprompter__settings-block form-field form-field--short">
+                <input class="teleprompter__input form-field__input">
+                <label class="teleprompter__label form-field__label">Скорость</label>
+              </li>
+              <li class="teleprompter__settings-block form-field form-field--short">
+                <input class="teleprompter__input form-field__input">
+                <label class="teleprompter__label form-field__label">Шрифт</label>
+              </li>
+            </ul>
+          </div>
+        </article>
+      </section>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../src/assets/scss/main.scss';
 </style>
